23dd8584251ef0770c94fc1e471fe811a56950e0
Nishant Kumar <nishantrajput1212@gmail.com>
2021-03-22 22:04:43 +0530

    Indentation fix

--- employeeservImplementation/src/main/java/com/paypal/bfs/test/employeeserv/entity/Employee.java
+++ employeeservImplementation/src/main/java/com/paypal/bfs/test/employeeserv/entity/Employee.java
@@ -33,61 +33,61 @@ public class Employee {
 	 * first name
 	 */
 	@Column(name = "first_name")
-    private String firstName;
-    /**
-     * last name
-     * (Required)
-     * 
-     */
+        private String firstName;
+        /**
+        * last name
+        * (Required)
+        * 
+        */
 	@Column(name = "last_name")
-    private String lastName;
-    /**
-     * date of birth
-     * (Required)
-     * 
-     */
+        private String lastName;
+        /**
+        * date of birth
+        * (Required)
+        * 
+         */
 	@Column(name = "dateOfBirth")
-    private Date dateOfBirth;
-    /**
-     * address line1
-     * (Required)
-     * 
-     */
+        private Date dateOfBirth;
+        /**
+        * address line1
+        * (Required)
+        * 
+        */
 	@Column(name = "address_line1")
-    private String addressLine1;
-    /**
-     * address line2
-     * 
-     */
+        private String addressLine1;
+        /**
+        * address line2
+        * 
+        */
 	@Column(name = "address_line2")
-    private String addressLine2;
-    /**
-     * address city
-     * (Required)
-     * 
-     */
+        private String addressLine2;
+        /**
+        * address city
+        * (Required)
+        * 
+        */
 	@Column(name = "city")
-    private String city;
-    /**
-     * address state
-     * (Required)
-     * 
-     */
+        private String city;
+        /**
+        * address state
+        * (Required)
+        * 
+        */
 	@Column(name = "state")
-    private String state;
-    /**
-     * address country
-     * (Required)
-     * 
-     */
+        private String state;
+        /**
+        * address country
+        * (Required)
+        * 
+        */
 	@Column(name = "country")
-    private String country;
-    /**
-     * address zip code
-     * (Required)
-     * 
-     */
+        private String country;
+        /**
+        * address zip code
+        * (Required)
+        * 
+        */
 	@Column(name = "zipcode")
-    private Integer zipCode;
+        private Integer zipCode;
 	
 }
